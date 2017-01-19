218133f836b3a500d1166e15b25422ae454d3127
Deepak <deepakkumar21120.dk@gmail.com>
2017-01-19 16:05:27 +0530

    rest_autt removed

--- flirtjarproject/urls.py
+++ flirtjarproject/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     url(r'^api/profile/', include('profiles.urls')),
     url(r'^api/location/', include('locations.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/auth/', include('rest_auth.urls')),
+    # url(r'^api/auth/', include('rest_auth.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     url(r'^api/docs/$', get_swagger_view(title='FlirtJar Api')),
